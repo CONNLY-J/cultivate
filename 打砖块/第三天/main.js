dcc32e5cce8a8ffed602d672b3ef2f9a11c5d778
liuju <1258948600@qq.com>
2020-05-28 23:25:10 +0800

    新增分数，优化砖块

---
+++ 打砖块/第三天/main.js
@@ -0,0 +1,104 @@
+ /*
+        * 定义图片来源路径
+        */
+       var imgPath = function(path){
+        var image = new Image();
+        image.src = path;
+        return image;
+    }
+
+var loadLevel = function(game,n){
+    n = n-1
+    var level = levels[n]
+    var blocks = []
+    for (var i=0;i<level.length;i++){
+        var p = level[i]
+        var b = Blocks(game,p)
+        blocks.push(b)
+    }
+    return blocks
+}
+        var blocks = []
+        var enableDebugMode = function(game,enable) {
+    if(!enable) {
+        return
+    }
+    window.paused = false
+    window.addEventListener('keydown', function(event){
+        var k = event.key
+        if (k == 'p') {
+            // 暂停功能
+            window.paused = !window.paused
+        } else if ('1234567'.includes(k)) {
+           //关卡功能
+            blocks = loadLevel(game,Number(k))
+        }
+    })
+    // 滑动进度条控制小球移动的速度
+    document.querySelector('#id-input').addEventListener('input', function(event) {
+        var input = event.target
+        console.log(event, input.value)
+        window.fps = Number(input.value)
+    })
+}
+var main = function(){
+    var images = {
+        ball:'ball.png',
+        brick:'brick.jpg',
+        board:'board.png'
+    }
+    
+    
+    var game = PlayGame(30,images,function(g){
+        var board = Board(game)
+        var ball = Ball(game);
+        var score = 0
+        // var block = Blocks()
+        blocks = loadLevel(game,1)
+        var paused = false
+        game.action('a', function(){
+        board.moveLeft()
+    })
+        game.action('d', function(){
+        board.moveRight()
+    })
+        game.action('f', function(){
+        ball.move()
+    })
+        game.update = function() {
+        if(window.paused){
+        return
+    }
+        ball.play()
+
+    //砖块和小球碰撞的处理
+        if (board.crash(ball)) {
+         ball.speedY *= -1
+     }
+        for (var i = 0; i < blocks.length; i++) {
+            var block = blocks[i]
+            if (block.crash(ball)) {
+            block.kill()
+            ball.rev()
+            score += 200
+    }
+}
+
+}
+game.draw = function() {
+game.drawImage(board)
+game.drawImage(ball)
+
+//通过循环遍历，话砖块
+for (var i = 0; i < blocks.length; i++) {
+    var block = blocks[i]
+    if (block.alive) {
+        game.drawImage(block)
+    }
+}
+game.context.fillText('分数:'+score,10,10)
+}
+    });
+    enableDebugMode(game,true)
+}
+main()
